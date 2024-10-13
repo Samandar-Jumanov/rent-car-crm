ba0ee780af873cc808e8991ed09be23597d8e0fa
Samandar Jumanov <samandarjumanov055@gmail.com>
2024-10-13 20:45:26 +0300

    Updated api url

--- utils/axios.ts
+++ utils/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 import { AUTH_COOKIE_NAME } from '@/lib/hooks/useAuth';
 
-export const API_BASE_URL = 'https://rent-car-3.onrender.com';
+export const API_BASE_URL = 'https://rent-car-4.onrender.com';
 export const local = "http://localhost:8080"
 
 const apiClient = axios.create({
